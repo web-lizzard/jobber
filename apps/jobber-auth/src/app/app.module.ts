8c754b9b6738da8e7d5f1d633f3970bee799a53d
weblizzard <czarnous.adrian@gmail.com>
2025-11-02 23:09:49 +0100

    chore: Introducing config module

--- apps/jobber-auth/src/app/app.module.ts
+++ apps/jobber-auth/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { PrismaModule } from './prisma/prisma.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { UserModule } from './users/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule,
     PrismaModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: true,
--- apps/jobber-auth/src/main.ts
+++ apps/jobber-auth/src/main.ts
@@ -7,11 +7,14 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 4000;
+  const port = configService.getOrThrow('AUTH_PORT');
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
   Logger.log(
