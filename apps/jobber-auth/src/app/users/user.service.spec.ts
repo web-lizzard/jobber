45a88caed02dbc0e0b3ac80a7563b060a47a70c4
weblizzard <czarnous.adrian@gmail.com>
2025-11-02 22:59:13 +0100

    test: Fixing invalid tests

--- apps/jobber-auth/src/app/users/user.service.spec.ts
+++ apps/jobber-auth/src/app/users/user.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: PrismaService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
